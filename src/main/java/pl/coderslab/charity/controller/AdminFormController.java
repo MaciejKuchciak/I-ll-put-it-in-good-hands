345c847dc49be15fc90b165792119acc1de84ab3
MaciejKuchciak <kuchciak.maciej@gmail.com>
2021-01-13 19:39:42 +0100

    9 Users management :: added feature to edit users

--- src/main/java/pl/coderslab/charity/controller/AdminFormController.java
+++ src/main/java/pl/coderslab/charity/controller/AdminFormController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.app.SecurityUtils;
+import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.entity.User;
 import pl.coderslab.charity.service.UserRolesService;
 import pl.coderslab.charity.service.UserService;
@@ -15,28 +16,46 @@ import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("admin")
-public class UserFormController {
+public class AdminFormController {
 
     private final UserService userService;
     private final UserRolesService userRolesService;
 
     @Autowired
-    public UserFormController(UserService userService, UserRolesService userRolesService) {
+    public AdminFormController(UserService userService, UserRolesService userRolesService) {
         this.userService = userService;
         this.userRolesService = userRolesService;
     }
 
-    @GetMapping("userslist/edit")
-    public String editUser(Model model, Long id){
-        User loggedAdmin = userService.getByEmail(SecurityUtils.username());
-        model.addAttribute("userFirstName",loggedAdmin.getFirstName());
-        User user = userService.getById(id);
-        model.addAttribute("user",user);
-        return "admin/edit-user";
+    @GetMapping("adminslist/add")
+    public String addNewAdmin(Model model){
+        User user = userService.getByEmail(SecurityUtils.username());
+        model.addAttribute("userFirstName",user.getFirstName());
+        model.addAttribute("admin", new User());
+        return "admin/new-admin";
     }
 
-    @PostMapping("userslist/edit")
-    public String userEditDone(User userInput){
+    @PostMapping("adminslist/add")
+    public String adminAdditionDone(User user){
+        user.setUserRoles(userRolesService.getAllUserRoles().get(1));
+        user.setCreated(LocalDateTime.now());
+        user.setLast_update(LocalDateTime.now());
+        user.setActive(true);
+        userService.add(user);
+        return "redirect:/admin/adminslist";
+    }
+
+    @GetMapping("adminslist/edit")
+    public String editAdmin(Model model, Long id){
+        User user = userService.getByEmail(SecurityUtils.username());
+        model.addAttribute("userFirstName",user.getFirstName());
+        User admin = userService.getById(id);
+        model.addAttribute("admin",admin);
+        return "admin/edit-admin";
+    }
+
+    @PostMapping("adminslist/edit")
+    public String adminEditDone(User userInput){
         User user = userService.getById(userInput.getId());
         //TODO: email and pw to be changed via e-mail, username should not be in the app
 //        user.setUsername(user.getUsername());
@@ -46,7 +65,7 @@ public class UserFormController {
 //        user.setPassword(userInput.getPassword());
         user.setLast_update(LocalDateTime.now());
         userService.edit(user);
-        return "redirect:/admin/userslist";
+        return "redirect:/admin/adminslist";
     }
 
 }
